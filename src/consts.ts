d414beddfdb0f51d1c00ebe019040cf25a9daa0f
Marco Pegoraro <marco.pegoraro@sinch.com>
2026-02-12 14:16:00 +0100

    feat: Revamp homepage to display dynamic blog posts with a new grid layout and redesign the site header with updated navigation.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Marco Pegoraro';
+export const SITE_DESCRIPTION = 'When it comes to write code you don\'t need to learn magic spells. Just take a look outside the window and solve your problems with patterns from the real world.';
